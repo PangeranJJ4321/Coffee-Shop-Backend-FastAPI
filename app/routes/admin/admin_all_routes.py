ae6e1d7d0c4d90a677e4ca97dc598cd5be1b5e35
PangeranJJ4321 <pangeranjuhrifar@gmail.com>
2025-05-23 14:35:00 +0800

    add router admin (booking status routes, admin order management, admin analitic statistic

--- app/routes/admin/admin_all_routes.py
+++ app/routes/admin/admin_all_routes.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter
-from app.routes.admin import admin_booking_management_routes, admin_coffee_management_routes
+from app.routes.admin import (
+    admin_booking_management_routes, 
+    admin_coffee_management_routes,
+    booking_status_routes,
+    admin_order_management_routes,
+    admin_analitics_statistics
+)
 
 router = APIRouter()
 
 router.include_router(admin_coffee_management_routes.router)
 router.include_router(admin_booking_management_routes.router)
+router.include_router(booking_status_routes.router)
+router.include_router(admin_booking_management_routes.router)
+router.include_router(admin_analitics_statistics.router)
